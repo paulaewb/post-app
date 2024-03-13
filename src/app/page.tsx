34d240a6cd9b77f6b9d5bf609e86b68d64945de8
Paula <mpaulaestradew@gmail.com>
2024-03-13 10:41:48 -0300

    localforage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,37 @@
 import { useContext, useEffect } from "react";
 import { usePosts } from "@/context/PostContext";
 import PostCard from "@/components/PostCard";
-
+import localforage from 'localforage';
 
 function HomePage() {
-  const {posts, loadPosts} = usePosts();
-  useEffect (() => {
-    loadPosts()
+  const { posts, loadPosts } = usePosts();
+
+  useEffect(() => {
+    // Check if data is available in local storage
+    localforage.getItem('posts').then((cachedPosts) => {
+      if (cachedPosts) {
+        // If data is available, use it
+        loadPosts(cachedPosts);
+      } else {
+        // If data is not available, load from API
+        loadPosts();
+      }
+    }).catch((error) => {
+      console.error('Error loading posts from local storage:', error);
+      // If error occurs, load from API
+      loadPosts();
+    });
   }, []);
-  return(
+
+  return (
     <div style={{ margin: '20%' }}>
       {
         posts.map(post => (
-          <PostCard post= {post} key = {post.id}></PostCard>
+          <PostCard post={post} key={post.id}></PostCard>
         ))
       }
     </div>
   )
-  
 }
 
-export default HomePage
+export default HomePage;
